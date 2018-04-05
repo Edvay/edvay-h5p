28a0b0e56a0b0625bd4c6679bd24db2ed3196240
Vinayak <vinayak.g@outlook.com>
2018-04-05 15:58:06 +0530

    added ganges heading

--- admin/views/edvay-h5p.php
+++ admin/views/edvay-h5p.php
@@ -1,12 +1,35 @@
 <?php
-/**
- * Select from all H5P content.
- *
- * @package   H5P
- * @author    Joubel <contact@joubel.com>
- * @license   MIT
- * @link      http://joubel.com
- * @copyright 2014 Joubel
- */
+$example = 'example';
+$wellArray = array (
+'Arrays are a lot of fun.',
+'Bootstrap is an amazing development tool to use with PHP',
+'With bootstrap you can quickly code and design beautiful websites'
+);
 ?>
-<div> <p>hello h5p</p></div>
+<html>
+<head>
+<title>PHP in HTML Example</title>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+</head>
+<body>
+
+     <header className="row small-header justify-content-center">
+     
+             <div className="logo pull-left">
+                  <a href="/"> <img src={logo} className="small-logo" alt="logo" />
+                   <h3 className="App-title small-margin">Ganges</h3></a>
+              </div>
+
+    
+              <div className="header_right pull-right">
+                 
+                    <a href="" className="btn btn-block ganges_btn">Deepak</a>
+                
+              </div>
+            
+     
+     
+        </header>
+</body>
+</html>
