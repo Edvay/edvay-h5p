69ed60dd4bb994a1d72f7547d1e51032f10c3e2d
Vinayak <vinayak.g@outlook.com>
2018-04-06 11:31:43 +0530

    debug commit3

--- admin/views/edvay-h5p.php
+++ admin/views/edvay-h5p.php
@@ -3,7 +3,7 @@ require_once('../class-h5p-plugin-admin.php');
 require_once('../class-h5p-content-admin.php');
 require_once('../class-h5p-library-admin.php');
 require_once('../../public/class-h5p-plugin.php');
-// $plugin = H5P_Plugin::get_instance();
+$plugin = H5P_Plugin::get_instance();
 // $plugin_slug = $plugin->get_plugin_slug();
 // $content = new H5PContentAdmin($this->plugin_slug);
 // $library = new H5PLibraryAdmin($this->plugin_slug);
