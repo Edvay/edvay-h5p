a0306c975db4ba9b1fe53bc6412f1852ab970041
Vinayak <vinayak.g@outlook.com>
2018-04-09 10:28:16 +0530

    trial commit

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+require_once plugin_dir_path(__FILE__) . 'autoloader.php';
+// $plugin = H5P_Plugin::get_instance();
+// // $plugin_slug = $plugin->get_plugin_slug();
+$content = new H5PContentAdmin('h5p');
+$library = new H5PLibraryAdmin('h5p');
+
+?>
+<html>
+<head>
+<title>PHP in HTML Example</title>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+<link rel='stylesheet' type='text/css' href='/wp-content/plugins/edvay-h5p/admin/styles/App.css'>
+</head>
+<body>
+
+   <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Ganges</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Interactive Video</a></li>  
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Deepak</a></li>     
+    </ul>
+  </div>
+ 
+</nav>
+</body>
+</html>
